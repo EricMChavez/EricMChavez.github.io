20fb95958fa4833b91d45fd6dac393e6cd8bedbb
Eric Chavez <eric@emcaudio.com>
2019-09-17 20:36:17 -0500

    Updates

---
+++ precache-manifest.2b5071cc48a22862c66ee49c9abd61c0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5af0facd095138e39d9bfd5551886d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83a6489ba55e7fcee760",
+    "url": "/static/css/main.b22d54f0.chunk.css"
+  },
+  {
+    "revision": "99bc2e7edc79dc4ae417",
+    "url": "/static/js/2.bd47b9d9.chunk.js"
+  },
+  {
+    "revision": "83a6489ba55e7fcee760",
+    "url": "/static/js/main.8f8155d3.chunk.js"
+  },
+  {
+    "revision": "a4d0e6d11719682b8aac",
+    "url": "/static/js/runtime~main.c0e1757a.js"
+  },
+  {
+    "revision": "1c8930521adc7a728dc4c0f243db6675",
+    "url": "/static/media/face.1c893052.jpg"
+  },
+  {
+    "revision": "3867bbad5dd35ed10dec709c739e18d3",
+    "url": "/static/media/logo-color.3867bbad.svg"
+  },
+  {
+    "revision": "935d2c47db8ac10a4439bcebe1607e6a",
+    "url": "/static/media/responsive.935d2c47.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.24a6a194cb233bd94c7ccf5a0772a4e5.js"
+  "/precache-manifest.2b5071cc48a22862c66ee49c9abd61c0.js"
 );
 
 self.addEventListener('message', (event) => {
