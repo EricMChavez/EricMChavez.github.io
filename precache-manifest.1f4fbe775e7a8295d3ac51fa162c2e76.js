55ca1fc11b49fcb87fc630767adc8f6c03f9f76e
Eric Chavez <eric@emcaudio.com>
2019-09-18 14:35:15 -0500

    Updates

--- precache-manifest.1f4fbe775e7a8295d3ac51fa162c2e76.js
+++ precache-manifest.1f4fbe775e7a8295d3ac51fa162c2e76.js
@@ -1,23 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "52e583e4dbacdcdd1b0fd979c760c10d",
+    "revision": "7647c0140468016c1294db74fbd05689",
     "url": "/index.html"
   },
   {
-    "revision": "88fb4914d549a57238ca",
-    "url": "/static/css/main.3fd98af9.chunk.css"
+    "revision": "90df665e189309addc80",
+    "url": "/static/css/main.d8a8c30a.chunk.css"
   },
   {
     "revision": "79a0d77739bd2e200382",
     "url": "/static/js/2.99be0f4c.chunk.js"
   },
   {
-    "revision": "88fb4914d549a57238ca",
-    "url": "/static/js/main.cdbbb459.chunk.js"
+    "revision": "90df665e189309addc80",
+    "url": "/static/js/main.90cf79f4.chunk.js"
   },
   {
     "revision": "a4d0e6d11719682b8aac",
     "url": "/static/js/runtime~main.c0e1757a.js"
+  },
+  {
+    "revision": "9be4064b6e5df73e143756237ca6338f",
+    "url": "/static/media/ericchavezresumé.9be4064b.pdf"
   },
   {
     "revision": "1c8930521adc7a728dc4c0f243db6675",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.22f9e11a1068cd632f92da9cb34310fb.js"
+  "/precache-manifest.1f4fbe775e7a8295d3ac51fa162c2e76.js"
 );
 
 self.addEventListener('message', (event) => {
