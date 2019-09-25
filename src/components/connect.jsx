4318223d59045080e10935f3977d5f98f0a20751
Eric Chavez <eric@emcaudio.com>
2019-09-25 02:37:00 -0500

    added modal

--- src/components/connect.jsx
+++ src/components/connect.jsx
@@ -28,15 +28,14 @@ class Connect extends Component {
         }else if(this.props.atTop){
             formClassName = 'hidden'
         }
-        const connectBtn = <div onClick={this.toggleConnect} id="connectBtn">CONNECT</div>
-        const connectForm = <form className={isConnectOpen ? "open" : 'null'} id="connectForm">
-                Form
-                <div id="closeBtn" onClick={this.toggleConnect}>X</div>
-            </form>
         return ( 
-            <div id="connect" className={formClassName}>
-                {isConnectOpen ? connectForm : connectBtn}
+            <React.Fragment>
+            <div id="greyScreen" className={isConnectOpen ? null : "greyClosed"}/>
+            <div onClick={this.toggleConnect} className={formClassName} id="connectBtn">CONNECT</div>
+            <div id="connectForm" className={isConnectOpen ? null : "greyClosed"}>
+            <svg onClick={this.toggleConnect} id="closeBtn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z" fill="#000"/></svg>
             </div>
+            </React.Fragment>
         );
     }
 }
