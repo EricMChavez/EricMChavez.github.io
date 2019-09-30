450dc1581147e69f08609b167e155a629853c234
Eric Chavez <eric@emcaudio.com>
2019-09-30 12:19:31 -0500

    changed write recommendation to btn

--- src/components/reviewForm.jsx
+++ src/components/reviewForm.jsx
@@ -1,62 +1,18 @@
 import React, { Component } from 'react';
-import axios from 'axios'
 import '../css/reviewForm.css'
 
-const url = "https://goofy-hugle-884571.netlify.com/.netlify/functions/reviews";
-
-const axiosConfig = {
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-    }
-};
 
 class ReviewForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            fullname: '',
-            jobTitle: '',
-            reviewBody: '',
-        }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleChange = (e) => {
-        this.setState({ [e.target.name]: e.target.value });
-    }
-    handleSubmit(e) {
-        const { fullname, jobTitle, reviewBody } = this.state
-        e.preventDefault();
 
-        const data = JSON.stringify({name: fullname, jobTitle:jobTitle, reviewBody:reviewBody});
-
-        axios.post(url, data, axiosConfig)
-        .then(res => {console.dir(res)})
-        .catch(err => {console.dir(err)})
-
-        this.setState({fullname: '', jobTitle: '',reviewBody: ''})
-        document.getElementById("reviewForm").reset()
-    }
     render() { 
-        const { fullname, jobTitle, reviewBody } = this.state
         return ( 
-            <form  onSubmit={this.handleSubmit} id="reviewForm">
-                <div id="fullName">
-                    <label htmlFor="fullname">Full Name</label>
-                    <input value={fullname} onChange={this.handleChange} placeholder="Required" name="fullname" id="fullNameField" type="text"></input>
-                </div>
-                <div id="jobTitle">
-                    <label htmlFor="jobTitle">Job Title</label>
-                    <input value={jobTitle} onChange={this.handleChange} name="jobTitle"  type="text"/>
-                </div>
-                <div id="recommendation">
-                    <textarea value={reviewBody} placeholder="Say Something Nice" onChange={this.handleChange} name="reviewBody" type="text"/>
-                </div>
-                <div id="linkedIn"><p>...or consider recommending me on</p><a href="https://www.linkedin.com/in/eric-chavez-253b0895"><div id="linkedInIcon"></div></a></div>
-                <button type="submit">SEND</button>
-            </form>
+            <div id="reviewPage">
+                <a href="https://www.linkedin.com/in/eric-chavez-253b0895/detail/recommendation/write/">
+                    <div id="reviewBtn">
+                        <div>CONSIDER RECOMMENDING ME ON</div><div id="linkedInIcon"></div>
+                    </div>
+                </a>
+            </div>
         );
     }
 }
--- src/components/reviews.jsx
+++ src/components/reviews.jsx
@@ -19,13 +19,6 @@ class Reviews extends Component{
 
         } 
     }
-    setFocus = () => {
-        setTimeout(() => {
-            document.getElementById("fullNameField").focus()
-        }, 400);
-        
-        
-    }
     render(){
         const { mobile } = this.state
     return(
@@ -35,32 +28,39 @@ class Reviews extends Component{
         <CarouselProvider
         naturalSlideWidth={mobile ? (300) :(800)}
         naturalSlideHeight={mobile ? (580) :(260)}
-        totalSlides={3}
+        totalSlides={4}
         dragEnabled={false}
         >
         <div id="reviews" className="shadow">
             <div id="reviewsHeader">
                 <div id="reviewsTitle">Recommendations</div>
-                <ButtonLast onClick={this.setFocus}  className="reviewsBtn">WRITE RECOMMENDATION</ButtonLast>
+                <ButtonLast className="reviewsBtn">WRITE RECOMMENDATION</ButtonLast>
             </div>
             
         <div className="slider">
             <Slider>
             <Slide className="slide" index={0}>
                 <div className="review">
-                    <div className="reviewPic trinity"/>
-                    <div className="reviewTitle"><div className="reviewName">Trinity Siemer</div>Teachers Assistant at Inventive Academy, LLC and Associate Software Engineer for Inventive Group</div>
-                    <div className="reviewWords">"I TA'ed the academy that Eric went through and he amazed me and all the other teachers and TA's the entire 6 months showing great determination and ambition even making the culture of the class amazing with his wittiness. We all think he would make a great addition to any team!"</div>
+                    <a rel="noopener noreferrer" target='_blank' href="https://www.linkedin.com/in/andrewsiemer" className="reviewPic andy"/>
+                    <div className="reviewTitle"><div className="reviewName">Andy Siemer</div>Co-Founder of Inventive - CEO</div>
+                    <div className="reviewWords">"Eric was a rockstar in our Inventive Academy bootcamp. We had our eyes set on hiring him straight away once he graduated. We immediately brought Eric into some of our projects in our consulting business and he was able to contribute to the team. Eric has the ability to pick up new concepts and run with them quickly. He is great to work with and he can be counted on to deliver great features. Eric is a wonderful addition to any scrum team!"</div>
                 </div>
             </Slide>
             <Slide className="slide" index={1}>
                 <div className="review">
-                    <div className="reviewPic james"/>
-                    <div className="reviewTitle"><div className="reviewName">James Shaw</div>co-founder of Inventive - COO, improving lives with technology</div>
-                    <div className="reviewWords">Eric is an enthusiastic learner and engineer - he has over-delivered and surprised us on a few occasions, giving us more than we hoped for! He was in our 6-month cohort and was dedicated throughout; certainly a star of the cohort and the first we hired. We're excited to have Eric on board and helping him gain experience in real-world projects.</div>
+                    <a rel="noopener noreferrer" target='_blank' href="https://www.linkedin.com/in/trinity-siemer-600587143" className="reviewPic trinity"/>
+                    <div className="reviewTitle"><div className="reviewName">Trinity Siemer</div>Teachers Assistant at Inventive Academy, LLC and Associate Software Engineer for Inventive Group</div>
+                    <div className="reviewWords">"I TA'ed the academy that Eric went through and he amazed me and all the other teachers and TA's the entire 6 months showing great determination and ambition even making the culture of the class amazing with his wittiness. We all think he would make a great addition to any team!"</div>
                 </div>
             </Slide>
-            <Slide className="slide" index={2}><ReviewForm/></Slide>
+            <Slide className="slide" index={2}>
+                <div className="review">
+                    <a rel="noopener noreferrer" target='_blank' href="https://www.linkedin.com/in/jamesshaw" className="reviewPic james"/>
+                    <div className="reviewTitle"><div className="reviewName">James Shaw</div>Co-Founder of Inventive - COO, Improving Lives with Technology</div>
+                    <div className="reviewWords">"Eric is an enthusiastic learner and engineer - he has over-delivered and surprised us on a few occasions, giving us more than we hoped for! He was in our 6-month cohort and was dedicated throughout; certainly a star of the cohort and the first we hired. We're excited to have Eric on board and helping him gain experience in real-world projects."</div>
+                </div>
+            </Slide>
+            <Slide className="slide" index={3}><ReviewForm/></Slide>
             </Slider>
             
             <ButtonBack className="backBtn slideBtn"></ButtonBack>
